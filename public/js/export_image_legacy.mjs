56b1af1e8d0bbc1adfa8f1dcf3e9fdf089766d06
Glitch (scandalous-citrine-banana) <none>
2021-05-20 18:31:24 +0000

    🍦🌷 Updated with Glitch

---
+++ public/js/export_image_legacy.mjs
@@ -0,0 +1,16 @@
+import { exportButton, toBlob } from './script.mjs';
+
+export const exportImage = async (blob) => {
+  const a = document.createElement('a');
+  a.download = 'fugu-greeting.png';
+  a.href = URL.createObjectURL(blob);
+  a.addEventListener('click', (e) => {    
+    setTimeout(() => URL.revokeObjectURL(a.href), 30 * 1000);
+  });
+  setTimeout(() => a.click(), 0);
+};
+
+exportButton.style.display = 'block';
+exportButton.addEventListener('click', async () => {
+  exportImage(await toBlob());
+});
